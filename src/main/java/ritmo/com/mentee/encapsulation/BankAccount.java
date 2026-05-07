9a68e7f36a6fd7a4625195393cceb40a0425d881
Kealeboga Lemeke <kelemeec025@student.wethinkcode.co.za>
2026-05-07 14:30:09 +0200

    my encapsulation project

--- src/main/java/ritmo/com/mentee/encapsulation/BankAccount.java
+++ src/main/java/ritmo/com/mentee/encapsulation/BankAccount.java
@@ -5,6 +5,32 @@ public class BankAccount {
     private double balance;
     private String ownerName;
 
+    public BankAccount(String acc,String name){
+        accountNumber = acc;
+        ownerName = name;
+    }
+    public String getAccountNumber(){
+        return accountNumber;
+    }
+    public double getBalance(){
+        return balance;
+    }
+    public String getOwnerName(){
+        return ownerName;
+    }
+    public void deposit(double amount){
+        if ( amount <=0) throw new IllegalArgumentException();
+        balance += amount;
+    }
+    public void deposit(int amount){
+        double newAmount = (double) amount;
+        deposit(newAmount);
+    }
+    public void withdraw(int amount){
+        if (amount <=0 || amount > balance) throw new IllegalArgumentException();
+        balance -= amount;
+    }
+
     @Override
     public String toString() {
         return "BankAccount{" +
--- src/main/java/ritmo/com/mentee/encapsulation/Person.java
+++ src/main/java/ritmo/com/mentee/encapsulation/Person.java
@@ -1,4 +1,41 @@
 package ritmo.com.mentee.encapsulation;
 
 public class Person {
+    private String name;
+    private int age;
+    private String email;
+
+    public  Person(String newName,int newAge,String newEmail){
+        name = newName;
+        age = newAge;
+        email = newEmail;
+    }
+    public String getName(){
+        return name;
+    }
+    public int getAge(){
+        return age;
+    }
+    public String getEmail(){
+        return email;
+    }
+    public Person(Person newPerson){
+        name = newPerson.name;
+        age = newPerson.age;
+        email = newPerson.email;
+    }
+    public Person(){}
+
+    public void setName(String newName){
+        if (newName == null || newName.isEmpty()) throw new IllegalArgumentException();
+        name = newName;
+    }
+    public void setAge(int newAge){
+        if (newAge < 0 || newAge >150) throw new IllegalArgumentException();
+        age = newAge;
+    }
+    public void setEmail(String newEmail){
+        if (!newEmail.contains("@")) throw new IllegalArgumentException();
+        email = newEmail;
+    }
 }
